01de5ec8b89b39c2f7c81e5c2c59fdc10b6977ae
neobotix <neobotix@example.com>
2022-08-17 13:06:47 +0200

    changed QoS for amcl_pose

--- src/neo_localization_node.cpp
+++ src/neo_localization_node.cpp
@@ -54,6 +54,20 @@ using std::placeholders::_2;
  *
  */
 
+
+ static const rmw_qos_profile_t rmw_qos_profile_amcl_pose_qos =
+ 	{
+		RMW_QOS_POLICY_HISTORY_KEEP_LAST,
+		10,
+		RMW_QOS_POLICY_RELIABILITY_RELIABLE,
+		RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL,
+		RMW_QOS_DEADLINE_DEFAULT,
+		RMW_QOS_LIFESPAN_DEFAULT,
+		RMW_QOS_POLICY_LIVELINESS_SYSTEM_DEFAULT,
+		RMW_QOS_LIVELINESS_LEASE_DURATION_DEFAULT,
+		false
+ 	};
+
 class NeoLocalizationNode : public rclcpp::Node {
 public:
 	NeoLocalizationNode(): Node("neo_localization_node")
@@ -160,7 +174,8 @@ public:
     	this->declare_parameter<bool>("broadcast_info", false);
     	this->get_parameter("broadcast_info", m_broadcast_info);
 
-		qos = rclcpp::QoS(rclcpp::KeepLast(1), rmw_qos_profile_amcl_pose_qos);
+		rmw_qos_profile_t amcl_qos = rmw_qos_profile_amcl_pose_qos;
+		auto qos = rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(amcl_qos));
 
 		m_map_update_thread = std::thread(&NeoLocalizationNode::update_loop, this);
 
@@ -790,21 +805,6 @@ private:
 	std::thread m_map_update_thread;
 	bool m_broadcast_info;
 	rclcpp::TimerBase::SharedPtr m_loc_update_timer;
-
-	static const rmw_qos_profile_t rmw_qos_profile_amcl_pose_qos =
- 	{
-		RMW_QOS_POLICY_HISTORY_KEEP_LAST,
-		10,
-		RMW_QOS_POLICY_RELIABILITY_RELIABLE,
-		RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL,
-		RMW_QOS_DEADLINE_DEFAULT,
-		RMW_QOS_LIFESPAN_DEFAULT,
-		RMW_QOS_POLICY_LIVELINESS_SYSTEM_DEFAULT,
-		RMW_QOS_LIVELINESS_LEASE_DURATION_DEFAULT,
-		false
- 	};
-
-	auto qos;
 };
 
 
